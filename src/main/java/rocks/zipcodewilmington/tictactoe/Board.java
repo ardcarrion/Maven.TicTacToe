02c1e113ad86c4cbdc027a428662986fa4eb9987
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-05-29 13:41:06 -0400

    written except for getWinner

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -1,6 +1,7 @@
 package rocks.zipcodewilmington.tictactoe;
 
 import com.sun.org.apache.xpath.internal.operations.Bool;
+import java.util.ArrayList;
 
 /**
  * @author leon on 6/22/18.
@@ -25,28 +26,10 @@ public class Board {
     }
 
     public String getWinner() {
-        //Character[] leftDiagonal = new Character[this.board.length];
-        //Character[] rightDiagonal = new Character[this.board.length];
-        Integer colCount = 0;
-        Integer rowCount = 0;
-        //Integer diagonalCount = 0;
-        String winner;
-        for (Character[] row : this.board) {
-            winner = winningSet(row);
-            if (!winner.equals("")) return winner;
-//            for (Character move : row) {
-//                cols[rowCount][colCount] = move;
-//                //rightDiagonal[diagonalCount] = this.board[colCount][rowCount];
-//                colCount++;
-//            }
-            //winner = winningSet(cols[rowCount]);
-            //if (!winner.equals("")) return winner;
-            //leftDiagonal[diagonalCount] = this.board[rowCount][colCount];
-            colCount = 0;
-            //rowCount++;
-        }
+        
         return "";
     }
+
 
     private String winningSet(Character[] moves) {
         int xCount = 0;
